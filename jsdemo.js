1b6215c5a1d1189a874533c96970dc28c8df3def
Isa-the-builder <Alphacarter2035@icloud.com>
2026-06-25 15:47:53 -0400

    Add variables and console logs to jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,1 +1,7 @@
 console.log("This is loaded from the externel JS file");
+var name = "Isaiah";
+let school = "Ardrey Kell High School"; 
+var fun_fact = "I am a person of many skills"; 
+console.log(`Name: ${name}`); 
+console.log(`School: ${school}`); 
+console.log(`Fun Fact: ${fun_fact}`)
