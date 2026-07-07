dd42b7c3d4b8ff8523d899958f57135e847cc8d8
Isa-the-builder <Alphacarter2035@icloud.com>
2026-07-07 16:40:15 -0400

    Fix longitude for CPCC coordinates
    Corrected the longitude value for CPCC.

--- basicmap.js
+++ basicmap.js
@@ -1,6 +1,6 @@
 // 35.2271° N latitude and -80.8431° W longitude
 var charlotte = [35.2271, -80.8431]; 
-var cpcc = [35.2176665, -80.314732];
+var cpcc = [35.2176665, -80.8314732];
 var map = L.map('map').setView(charlotte, 13); 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
