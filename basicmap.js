d3bb28b147a52f242f5ae87bf7dee4bfa59ef1e3
Isa-the-builder <Alphacarter2035@icloud.com>
2026-07-07 16:31:56 -0400

    Fix syntax error by adding semicolon

--- basicmap.js
+++ basicmap.js
@@ -1,5 +1,5 @@
 // 35.2271° N latitude and -80.8431° W longitude
-var charlotte = [35.2271, -80.8431] 
+var charlotte = [35.2271, -80.8431]; 
 var cpcc = [35.2176665, -80.314732];
 var map = L.map('map').setView(charlotte, 13); 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
