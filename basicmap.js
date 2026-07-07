5e869daef1b9ae85a4563139f59aba7d1b05dbb0
Isa-the-builder <Alphacarter2035@icloud.com>
2026-07-07 16:29:20 -0400

    Fix coordinates precision and add marker title

--- basicmap.js
+++ basicmap.js
@@ -1,9 +1,9 @@
 // 35.2271° N latitude and -80.8431° W longitude
 var charlotte = [35.2271, -80.8431] 
-var cpcc = [35.21766665, -80.314732];
+var cpcc = [35.2176665, -80.314732];
 var map = L.map('map').setView(charlotte, 13); 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var cpccmarker = L.marker(cpcc).addTo(map);
+var cpccmarker = L.marker(cpcc, {title:"CPCC Center Campus"}).addTo(map);
