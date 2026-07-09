b97aaa313b5fcaf8015c491ae56156f6071b53bc
Isa-the-builder <Alphacarter2035@icloud.com>
2026-07-09 15:53:21 -0400

    Fix popup binding in travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -1,5 +1,9 @@
 //40.749191° N, -73.967590° W 
 var coordinates =[40.749191, -73.967590]; 
+var popoupcode =`<center>
+<h3>Hello from the Maldives!</h3> 
+<iframe width="200" height="100" src="https://www.youtube.com/embed/vQ-IkgCCxq4?si=kLb7Sssu-YjMQq-_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</center>`;
 var map = L.map('map').setView(coordinates, 13); 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -8,4 +12,4 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.circle(coordinates, {"title": "Welcome to the UN Building!",
                       color: "blue", 
                       fillcolor: "#243eed", 
-                      radius: 90}).bindPopup("<p> Welcome to the UN Building!</p>").addTo(map);
+                      radius: 90}).bindPopup(popupcode).addTo(map);
