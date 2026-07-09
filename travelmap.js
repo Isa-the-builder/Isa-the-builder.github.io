5518d886126f5fe4786d2e74aea7489e38eec3d1
Isa-the-builder <Alphacarter2035@icloud.com>
2026-07-09 15:41:06 -0400

    Change circle color from black to blue

--- travelmap.js
+++ travelmap.js
@@ -6,6 +6,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map); 
 L.circle(coordinates, {"title": "Welcome to the UN Building!",
-                      color: "black", 
+                      color: "blue", 
                       fillcolor: "#243eed", 
                       radius: 90}).bindPopup("<p> Welcome to the UN Building!</p>").addTo(map);
