2a46f77d7bc79e87f298f10703adb0eba86ea43c
Isa-the-builder <Alphacarter2035@icloud.com>
2026-07-09 16:13:00 -0400

    Add marker for Roosevelt Island with popup

--- travelmap.js
+++ travelmap.js
@@ -3,7 +3,14 @@ var coordinates =[40.749191, -73.967590];
 var popupcode =`<center>
 <h3>Hello from the Maldives!</h3> 
 <iframe width="200" height="100" src="https://www.youtube.com/embed/vQ-IkgCCxq4?si=kLb7Sssu-YjMQq-_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-</center>`;
+</center>`; 
+var marker1 = [40.761944, -73.950000]; 
+var markercode = `<center>
+<h3>Roosevelt Island</h3>  
+<a href="travel.jpg">
+<img src="travel.jpg" title="Roosevelt Island" height="100" width= "200"> 
+</a>
+</center>`
 var map = L.map('map').setView(coordinates, 13); 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -13,3 +20,4 @@ L.circle(coordinates, {"title": "Welcome to the UN Building!",
                       color: "blue", 
                       fillcolor: "#243eed", 
                       radius: 90}).bindPopup(popupcode).addTo(map);
+L.marker(marker1).bindPopup(markercode).addTo(map);
